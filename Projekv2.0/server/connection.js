fe73e9c95dc3c758985cc4091eb453cc65f60307
Elsy <74521237+dotcom-a@users.noreply.github.com>
2024-12-14 18:56:19 +0700

    Update connection.js

--- Projekv2.0/server/connection.js
+++ Projekv2.0/server/connection.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     host: "aws-0-ap-southeast-1.pooler.supabase.com",
     user: "postgres.afnjcdvtxlasvmbsqtfm",
     database: "postgres",
-    password: "MjBGtA1eFuxqoKOJ",
+    password: "***",
     port: "6543",
     max: 20,
     idleTimeoutMillis: 2000,
